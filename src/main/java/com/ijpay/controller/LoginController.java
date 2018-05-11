7e59983d3c5593f98e1430524f7113163e9662cf
fengzhiyu <13269105873@163.com>
2018-05-11 09:40:47 +0800

    删除无效代码

--- src/main/java/com/ijpay/controller/LoginController.java
+++ src/main/java/com/ijpay/controller/LoginController.java
@@ -4,10 +4,8 @@ import com.alibaba.fastjson.JSON;
 import com.util.OkHttpUtil;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.util.Map;
 
 /**
