bc10ad835ffd0f9158271e21ec800ed1f26e1628
fengzhiyu <13269105873@163.com>
2018-05-15 10:19:24 +0800

    补充文档

--- src/test/java/com/order/test/OrderControllerTest.java
+++ src/test/java/com/order/test/OrderControllerTest.java
@@ -57,7 +57,7 @@ public class OrderControllerTest extends SpringRestDocApplicationTests {
                                 subsectionWithPath("content[0].openid").description("用户唯一标识").type("String"),
                                 subsectionWithPath("content[0].orderNo").description("订单编号").type("String"),
                                 subsectionWithPath("content[0].orderStatus").description("订单状态,0未支付,1已取消,2已支付").type("int"),
-                                subsectionWithPath("content[0].payChannel").description("支付渠道").type("0微信,1支付宝"),
+                                subsectionWithPath("content[0].payChannel").description("支付渠道0微信,1支付宝").type("int"),
                                 subsectionWithPath("content[0].outTradeNo").description("第三方支付订单号").type("String"),
                                 subsectionWithPath("content[0].energyNum").description("购买的电量").type("BigDecimal"),
                                 subsectionWithPath("content[0].energySum").description("电量的价格").type("BigDecimal"),
@@ -112,6 +112,14 @@ public class OrderControllerTest extends SpringRestDocApplicationTests {
                                 fieldWithPath("energySum").description("总电量"),
                                 fieldWithPath("orderSum").description("订单金额"),
                                 fieldWithPath("openBill").description("是否开发票")
+                        ),
+                        responseFields(
+                                fieldWithPath("timeStamp").description("微信订单时间戳"),
+                                fieldWithPath("package").description("预支付订单编号"),
+                                fieldWithPath("paySign").description("支付签名"),
+                                fieldWithPath("appId").description("小程序id"),
+                                fieldWithPath("signType").description("签名的加密方式"),
+                                fieldWithPath("nonceStr").description("随机字符串")
                         )));
     }
 
@@ -126,7 +134,37 @@ public class OrderControllerTest extends SpringRestDocApplicationTests {
 
         mockMvc.perform(requestBuilder).
                 andExpect(status().isOk())
-                .andDo(document("restful-order-list"));
+                .andDo(document("restful-order-list",
+                        pathParameters(
+                                parameterWithName("pageNum").description("当前页数"),
+                                parameterWithName("pageSize").description("每页条数"),
+                                parameterWithName("openid").description("用户标识")
+                        ),
+                        responseFields(
+                                subsectionWithPath("content[0].id").description("主键标识").type("long"),
+                                subsectionWithPath("content[0].chargeId").description("充电信息主键").type("long"),
+                                subsectionWithPath("content[0].machineNo").description("充电桩编号").type("String"),
+                                subsectionWithPath("content[0].openid").description("用户唯一标识").type("String"),
+                                subsectionWithPath("content[0].orderNo").description("订单编号").type("String"),
+                                subsectionWithPath("content[0].orderStatus").description("订单状态,0未支付,1已取消,2已支付").type("int"),
+                                subsectionWithPath("content[0].payChannel").description("支付渠道0微信,1支付宝").type("int"),
+                                subsectionWithPath("content[0].outTradeNo").description("第三方支付订单号").type("String"),
+                                subsectionWithPath("content[0].energyNum").description("购买的电量").type("BigDecimal"),
+                                subsectionWithPath("content[0].energySum").description("电量的价格").type("BigDecimal"),
+                                subsectionWithPath("content[0].orderSum").description("订单支付的金额").type("BigDecimal"),
+                                subsectionWithPath("content[0].createtime").description("订单创建时间").type("Timestamp"),
+                                subsectionWithPath("content[0].paytime").description("订单支付时间").type("Timestamp"),
+                                subsectionWithPath("content[0].openBill").description("是否开发票,0不开,1开").type("int"),
+                                subsectionWithPath("content[0].billNum").description("发票编号").type("String"),
+                                subsectionWithPath("last").description("是否最后一页").type("boolean"),
+                                subsectionWithPath("totalPages").description("总页数").type("int"),
+                                subsectionWithPath("totalElements").description("总元素数").type("int"),
+                                subsectionWithPath("number").description("页数").type("int"),
+                                subsectionWithPath("size").description("页面大小").type("int"),
+                                subsectionWithPath("sort").description("排序方式").type("Object"),
+                                subsectionWithPath("first").description("是否第一页").type("boolean"),
+                                subsectionWithPath("numberOfElements").description("当前页元素数").type("int")
+                        )));
     }
 
     /**
@@ -140,7 +178,27 @@ public class OrderControllerTest extends SpringRestDocApplicationTests {
 
         mockMvc.perform(requestBuilder)
                 .andExpect(status().isOk())
-                .andDo(document("restful-order-getbyid"));
+                .andDo(document("restful-order-getbyid",
+                        pathParameters(
+                                parameterWithName("id").description("订单id")
+                        ),
+                        responseFields(
+                                subsectionWithPath("id").description("主键标识").type("long"),
+                                subsectionWithPath("chargeId").description("充电信息主键").type("long"),
+                                subsectionWithPath("machineNo").description("充电桩编号").type("String"),
+                                subsectionWithPath("openid").description("用户唯一标识").type("String"),
+                                subsectionWithPath("orderNo").description("订单编号").type("String"),
+                                subsectionWithPath("orderStatus").description("订单状态,0未支付,1已取消,2已支付").type("int"),
+                                subsectionWithPath("payChannel").description("支付渠道0微信,1支付宝").type("int"),
+                                subsectionWithPath("outTradeNo").description("第三方支付订单号").type("String"),
+                                subsectionWithPath("energyNum").description("购买的电量").type("BigDecimal"),
+                                subsectionWithPath("energySum").description("电量的价格").type("BigDecimal"),
+                                subsectionWithPath("orderSum").description("订单支付的金额").type("BigDecimal"),
+                                subsectionWithPath("createtime").description("订单创建时间").type("Timestamp"),
+                                subsectionWithPath("paytime").description("订单支付时间").type("Timestamp"),
+                                subsectionWithPath("openBill").description("是否开发票,0不开,1开").type("int"),
+                                subsectionWithPath("billNum").description("发票编号").type("String")
+                        )));
     }
 
     /**
@@ -163,7 +221,11 @@ public class OrderControllerTest extends SpringRestDocApplicationTests {
         /**mockMvc执行post请求*/
         mockMvc.perform(requestBuilder)
                 .andExpect(status().isOk())
-                .andDo(document("restful-order-editstatus"));
+                .andDo(document("restful-order-editstatus",
+                        requestFields(
+                                fieldWithPath("id").description("订单主键"),
+                                fieldWithPath("orderStatus").description("订单状态,0未支付,1已取消,2已支付")
+                        )));
 
     }
 }
